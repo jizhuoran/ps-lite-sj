cf8ced0c79f4e1fa0af914cb16483d61bd529899
sj <jizr@connect.hku.hk>
2017-10-02 18:44:40 +0800

    first wait method 3

--- tests/test_connection.cc
+++ tests/test_connection.cc
@@ -4,7 +4,8 @@ using namespace std;
 using namespace ps;
 #define ASYNC 1
 #define SYNC 2
-int method = 1;
+#define MAX_DIFF 2
+int method = 3;
 template <class Val>
 class KVServerDefaultHandle1 {      //functor，用与处理server收到的来自worker的请求
 public:
@@ -15,6 +16,7 @@ public:
         size_t n = req_data.keys.size();
         int work_id = (req_meta.sender - 9)/2;
         
+        std::cout << "worker id is " << work_id << "where it is push" << req_meta.push << std::endl;
         if (req_meta.push) { // push
             
         } else {            // pull
@@ -91,7 +93,95 @@ public:
                             server->Response(meta_queue[ticks[work_id] - last_tick][meta_queue_tick_itr], res);
                         }
                     }
-                }   
+                }else if (method == 3) {
+
+                    for(int vec_itr = 0; vec_itr < meta_vec.size(); ++vec_itr) {
+
+                        bool should_be_send = true;
+                        int work_itr_tick = ticks[(meta_vec[vec_itr].sender - 9)/2];
+                       
+                        for(int tick_itr = 0; tick_itr < NumWorkers(); ++tick_itr) {
+                            if(work_itr_tick - ticks[tick_itr] > MAX_DIFF) {
+                                should_be_send = false;
+                                break;
+                            }
+                        }
+
+                        if(should_be_send) {
+                            KVPairs<Val> res;
+                            res.keys = data_vec[vec_itr].keys;
+                            res.lens.resize(data_vec[vec_itr].keys.size());
+            
+                            res.lens[i] = grad[work_id].size();
+
+                            int recv_id = (meta_vec[vec_itr].sender - 9)/2;
+                            for(int idx = 0; idx < res.lens[i]; ++idx){
+                                
+                                
+                                res.vals.push_back(grad[recv_id][idx]);
+                                grad[recv_id][idx] = 0;
+                                                            
+                             }
+                            
+                            std::cout << "Now we send to worker" << recv_id << std::endl;
+                            std::cout << "s the ticks is ";
+                            for(auto v:ticks) {
+                                cout << v << "  ";
+                            }
+                            std::cout << std::endl;
+                            server->Response(meta_vec[vec_itr], res);
+
+                            std::cout << "before erase";
+                            for(auto v:meta_vec) {
+                                cout << v.sender << " ";
+                            }
+                            std::cout << std::endl;
+                            meta_vec.erase(meta_vec.begin() + vec_itr);
+                            data_vec.erase(data_vec.begin() + vec_itr);
+                            std::cout << "after erase";
+                            for(auto v:meta_vec) {
+                                cout << v.sender << " ";
+                            }
+                            std::cout << std::endl;
+                            
+                        }
+                    }
+
+
+
+
+
+
+
+                    int my_tick = ticks[work_id];
+
+                    for(int work_itr = 0; work_itr < NumWorkers(); work_itr++) {
+                        if((my_tick - ticks[work_itr]) > MAX_DIFF) {
+                            meta_vec.push_back(req_meta);
+                            data_vec.push_back(req_data);
+                            std::cout << "we need to wait as the ticks is ";
+                            for(auto v:ticks) {
+                                cout << v << "  ";
+                            }
+                            std::cout << std::endl;
+                            return;
+                        }
+                    }
+                    
+                    KVPairs<Val> res;
+                    res.keys = req_data.keys;
+                    res.lens.resize(res.keys.size());
+    
+                    res.lens[i] = grad[work_id].size();
+                    for(int idx = 0; idx < res.lens[i]; ++idx){
+                        res.vals.push_back(grad[work_id][idx]);
+                        grad[work_id][idx] = 0;
+                    }
+    
+                    server->Response(req_meta, res);
+
+
+                }
             }
         }
         
@@ -101,6 +191,9 @@ private:
     std::vector<int> ticks =  std::vector<int>(NumWorkers(), 0);
     std::vector<std::vector<KVMeta> > meta_queue;
     std::vector<std::vector<KVPairs<Val> > > data_queue;
+
+    std::vector<KVMeta> meta_vec;
+    std::vector<KVPairs<Val> > data_vec;
     int last_tick = 1;
 };
 
@@ -117,5 +210,5 @@ int main(int argc, char* argv[]) {
     StartServer();
     Start();    //启动,Postoffice::start()
     Finalize(); //结束。每个节点都需要执行这个函数。
-    return 0;
+    return 3;
 }
